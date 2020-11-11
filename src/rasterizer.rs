cd345d384bee4a362cff552279f577622fa6d094
Ivan Skorokhodov <iskorokhodov@gmail.com>
2020-11-11 15:05:58 +0300

    feat: gouraud shading

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -25,15 +25,7 @@ struct State {
     curr_mouse_y: f32,
     object_to_world: AffineMat3,
     arcball_enabled: bool,
-    // is_mouse_inited: bool,
-    // curr_mouse_x: f32,
-    // curr_mouse_y: f32,
-    // mouse_sensitivity: f32,
-    // move_speed: f32,
-    // mouse_is_in_window: bool,
-    // scroll_speed: f32,
-    // rotation_speed: f32,
-    // scale_speed: f32,
+    light_position: Point,
 }
 
 struct Camera {
@@ -167,18 +159,16 @@ fn update_on_event(app: &App, state: &mut State, event: Event) {
     if (state.curr_mouse_x == app.mouse.x && state.curr_mouse_y == app.mouse.y) {
         return;
     }
-    // println!("Mouse pos: {} {} {} {}", state.curr_mouse_x, state.curr_mouse_y, app.mouse.x, app.mouse.y);
+
     let prev_arcball_vec = Camera::compute_arcball_vector_for_xy(state.curr_mouse_x, state.curr_mouse_y);
     let curr_arcball_vec = Camera::compute_arcball_vector_for_xy(app.mouse.x, app.mouse.y);
     let angle = 2.0 * prev_arcball_vec.dot_product(&curr_arcball_vec).min(1.0).acos();
     let world_to_camera = state.camera.compute_view_matrix();
-    // let object_to_camera = &world_to_camera * &state.object_translation;
-    // let camera_to_object = &world_to_camera.compute_inverse();
-    let camera_to_world = &world_to_camera.compute_inverse();
+    let object_to_camera = &world_to_camera * &state.object_to_world;
+    let camera_to_object = &world_to_camera.compute_inverse();
     let axis_camera = &prev_arcball_vec.cross_product(&curr_arcball_vec);
-    // let axis_camera = &curr_arcball_vec.cross_product(&prev_arcball_vec);
-    let axis_world = (camera_to_world * axis_camera).normalize();
-    let rotation = AffineMat3::rotation(angle, &axis_world);
+    let axis_object = (camera_to_object * axis_camera).normalize();
+    let rotation = AffineMat3::rotation(angle, &axis_object);
 
     // state.object_rotation = &rotation * &state.object_rotation;
     state.object_to_world = &rotation * &state.object_to_world;
@@ -252,9 +242,8 @@ fn render_state(state: &State) -> DynamicImage{
     // );
     let world_to_camera = state.camera.compute_view_matrix();
     let object_to_camera = &world_to_camera * &state.object_to_world;
+    let light_pos_camera = &world_to_camera * &state.light_position;
     let camera_to_object = object_to_camera.compute_inverse();
-    // let image_width: usize = 1280;
-    // let image_height: usize = 960;
     let image_width: usize = WIDTH;
     let image_height: usize = HEIGHT;
 
@@ -262,13 +251,9 @@ fn render_state(state: &State) -> DynamicImage{
         film_aperture_width, film_aperture_width, image_width,
         image_height, near_clipping_plane, focal_len);
 
-    // Vec3<unsigned char> *frameBuffer = new Vec3<unsigned char>[image_width * image_height];
-    // let mut frame_buffer = [Color::new(0.0, 0.0, 0.0); (image_width * image_height) as usize];
-    // let mut z_buffer = [far_clipping_plane; (image_width * image_height) as usize];
-    let mut frame_buffer = vec![Color::new(1.0, 1.0, 1.0); image_width * image_height];
+    let mut frame_buffer = vec![Color::new(0.2, 0.2, 0.2); image_width * image_height];
     let mut z_buffer = vec![far_clipping_plane; image_width * image_height];
 
-    let mut counter = 0;
     let start = Instant::now();
     let mut preparation_time = Duration::from_secs(0);
     let mut inner_loop_time = Duration::from_secs(0);
@@ -283,6 +268,22 @@ fn render_state(state: &State) -> DynamicImage{
         let v0 = Point::new(model.mesh.positions[idx_1 * 3 + 0], model.mesh.positions[idx_1 * 3 + 1], model.mesh.positions[idx_1 * 3 + 2]);
         let v1 = Point::new(model.mesh.positions[idx_2 * 3 + 0], model.mesh.positions[idx_2 * 3 + 1], model.mesh.positions[idx_2 * 3 + 2]);
         let v2 = Point::new(model.mesh.positions[idx_3 * 3 + 0], model.mesh.positions[idx_3 * 3 + 1], model.mesh.positions[idx_3 * 3 + 2]);
+
+        // TODO: the best option would be to compute the normal and v_cam inside the first run...
+        let v0_camera = &object_to_camera * &v0;
+        let v1_camera = &object_to_camera * &v1;
+        let v2_camera = &object_to_camera * &v2;
+        let normal_camera = (&((&v1_camera - &v0_camera).cross_product(&(&v2_camera - &v0_camera)))).normalize();
+        let light_dirs = (
+            (&light_pos_camera - &v0_camera).normalize(),
+            (&light_pos_camera - &v1_camera).normalize(),
+            (&light_pos_camera - &v2_camera).normalize(),
+        );
+        let colors_gouraud = (
+            normal_camera.dot_product(&light_dirs.0),
+            normal_camera.dot_product(&light_dirs.1),
+            normal_camera.dot_product(&light_dirs.2),
+        );
 
         // Convert the vertices of the triangle to raster space
         let mut v0_raster = convert_to_raster(&v0, &object_to_camera, l, r, t, b, near_clipping_plane, image_width, image_height);
@@ -339,72 +340,54 @@ fn render_state(state: &State) -> DynamicImage{
 
         for y in y0..(y1 + 1) {
             for x in x0..(x1 + 1) {
-                let inner_loop_stuff_start = Instant::now();
                 let pixel_pos = Point::new(x as f32 + 0.5, y as f32 + 0.5, 0.0);
-                let mut w0 = edge_function(&v1_raster, &v2_raster, &pixel_pos);
-                let mut w1 = edge_function(&v2_raster, &v0_raster, &pixel_pos);
-                let mut w2 = edge_function(&v0_raster, &v1_raster, &pixel_pos);
-                inner_loop_time += inner_loop_stuff_start.elapsed();
+                let bar_coords = (
+                    edge_function(&v1_raster, &v2_raster, &pixel_pos) / area,
+                    edge_function(&v2_raster, &v0_raster, &pixel_pos) / area,
+                    edge_function(&v0_raster, &v1_raster, &pixel_pos) / area,
+                );
 
-                if (w0 >= 0.0 && w1 >= 0.0 && w2 >= 0.0) {
-                    w0 /= area;
-                    w1 /= area;
-                    w2 /= area;
-                    let one_div_z = v0_raster.z * w0 + v1_raster.z * w1 + v2_raster.z * w2;
-                    let z = 1.0 / one_div_z;
+                if (bar_coords.0 >= 0.0 && bar_coords.1 >= 0.0 && bar_coords.2 >= 0.0) {
+                    let depth = 1.0 / (v0_raster.z * bar_coords.0 + v1_raster.z * bar_coords.1 + v2_raster.z * bar_coords.2);
 
                     // Depth-buffer test
-                    if (z < z_buffer[y * image_width + x]) {
-                        z_buffer[y * image_width + x] = z;
+                    if (depth < z_buffer[y * image_width + x]) {
+                        z_buffer[y * image_width + x] = depth;
 
-                        let tex_coords = (
-                            (st0.0 * w0 + st1.0 * w1 + st2.0 * w2) * z,
-                            (st0.1 * w0 + st1.1 * w1 + st2.1 * w2) * z
-                        );
+                        // let tex_coords = (
+                        //     (st0.0 * bar_coords.0 + st1.0 * bar_coords.1 + st2.0 * bar_coords.2) * depth,
+                        //     (st0.1 * bar_coords.0 + st1.1 * bar_coords.1 + st2.1 * bar_coords.2) * depth
+                        // );
 
-                        // println!("Tex: {}, {}", tex_coords.0, tex_coords.1);
-
-                        // If you need to compute the actual position of the shaded
-                        // point in camera space. Proceed like with the other vertex attribute.
-                        // Divide the point coordinates by the vertex z-coordinate then
-                        // interpolate using barycentric coordinates and finally multiply by sample depth.
-                        let v0_cam = &world_to_camera * &v0;
-                        let v1_cam = &world_to_camera * &v1;
-                        let v2_cam = &world_to_camera * &v2;
-
-                        let px = (v0_cam.x/-v0_cam.z) * w0 + (v1_cam.x/-v1_cam.z) * w1 + (v2_cam.x/-v2_cam.z) * w2;
-                        let py = (v0_cam.y/-v0_cam.z) * w0 + (v1_cam.y/-v1_cam.z) * w1 + (v2_cam.y/-v2_cam.z) * w2;
-                        let pt = Point::new(px * z, py * z, -z); // pt is in camera space
+                        // let px = (v0_camera.x / -v0_camera.z) * bar_coords.0 + (v1_camera.x / -v1_camera.z) * bar_coords.1 + (v2_camera.x / -v2_camera.z) * bar_coords.2;
+                        // let py = (v0_camera.y / -v0_camera.z) * bar_coords.0 + (v1_camera.y / -v1_camera.z) * bar_coords.1 + (v2_camera.y / -v2_camera.z) * bar_coords.2;
+                        // let pos_camera = Point::new(px * depth, py * depth, -depth); // fragmet position is in the camera space
 
                         // Compute the face normal which is used for a simple facing ratio.
                         // Keep in mind that we are doing all calculation in camera space.
                         // Thus the view direction can be computed as the point on the object
                         // in camera space minus Vec3f(0), the position of the camera in camera space.
-                        let normal = (&((&v1_cam - &v0_cam).cross_product(&(&v2_cam - &v0_cam)))).normalize();
-                        let view_direction = (-&Vec3::new(pt.x, pt.y, pt.z)).normalize();
-                        let mut n_dot_view = normal.dot_product(&view_direction).max(0.0);
+                        // let view_direction = (-&Vec3::new(pos_camera.x, pos_camera.y, pos_camera.z)).normalize();
+                        // let mut n_dot_view = normal_camera.dot_product(&view_direction).max(0.0);
 
-                        // The final color is the reuslt of the faction ration multiplied by the
-                        // checkerboard pattern.
-                        let M = 10.0;
-                        let checker = (((tex_coords.0 * M) % 1.0) > 0.5) ^ (((tex_coords.1 * M) % 1.0) < 0.5);
-                        let c = if checker { 0.7 } else { 0.3 };
-                        n_dot_view *= c;
-                        frame_buffer[y * image_width + x].r = n_dot_view;
-                        frame_buffer[y * image_width + x].g = n_dot_view;
-                        frame_buffer[y * image_width + x].b = n_dot_view;
+                        // // The final color is the reuslt of the faction ration multiplied by the
+                        // // checkerboard pattern.
+                        // let M = 10.0;
+                        // let checker = (((tex_coords.0 * M) % 1.0) > 0.5) ^ (((tex_coords.1 * M) % 1.0) < 0.5);
+                        // let c = if checker { 0.7 } else { 0.3 };
+                        // n_dot_view *= c;
+                        let color = colors_gouraud.0 * bar_coords.0 + colors_gouraud.1 * bar_coords.1 + colors_gouraud.2 * bar_coords.2;
+
+                        frame_buffer[y * image_width + x] = Color::new(color, color, color);
                     }
                 }
             }
         }
     }
 
-    // println!("Counter: {}", counter);
-
     let duration = start.elapsed();
     // println!("Rasterizer done! Took time: {} ms", duration.as_millis());
     // println!("Preparation time took: {} ms", preparation_time.as_millis());
-    // println!("Inner loop time took: {} ms", inner_loop_time.as_millis());
 
     let mut img = RgbImage::new(image_width as u32, image_height as u32);
     for y in 0..image_height {
@@ -435,13 +418,7 @@ fn init_state(model: Model) -> State {
 
     State {
         model: model,
-        // object_to_world: AffineMat3::translation(Vec3::new(0.0, -5.0, 0.0)),
-        // object_to_world: AffineMat3::identity(),
-        // object_to_world: AffineMat3::translation((&-&(&object_center * 2.5)).into()),
-        // object_to_world: &AffineMat3::rotation(1.57, &Vec3::new(0.0, 1.0, 0.0)) * &AffineMat3::translation(((&-&object_center).into())),
         object_to_world: AffineMat3::translation((&-&(&object_center * 2.5)).into()),
-        // object_rotation: AffineMat3::identity(),
-        // object_to_world: AffineMat3::rotation(1.57, &Vec3::new(0.0, 1.0, 0.0)),
         camera: Camera {
             distance: distance,
             eye: Point::new(0.0, 0.0, -distance),
@@ -451,6 +428,7 @@ fn init_state(model: Model) -> State {
         curr_mouse_x: 0.0,
         curr_mouse_y: 0.0,
         arcball_enabled: false,
+        light_position: Point::new(0.0, 100.0, 0.0),
     }
 }
 
